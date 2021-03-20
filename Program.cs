19b86ba83ad6c5ee1ee343e2a8871deec34c3d19
tiveritz <lukas.walch@icloud.com>
2021-03-20 20:27:00 +0100

    [Refactor]

--- Program.cs
+++ Program.cs
@@ -10,20 +10,31 @@ namespace csharp_mysql_connection
             Console.WriteLine("Hello World C# .NET MySql");
             Console.WriteLine("-------------------------");
 
-            MySqlConnector mySql = new MySqlConnector("127.0.0.1", "coding_campus", "coding_campus", "coding_campus_pass");
+            // Use a custom MySQL class -> hide connection data in production!
+            MySqlConnector mySql = new MySqlConnector("127.0.0.1", "develop", "develop", "develop_pass");
 
-            mySql.Query(
-                "CREATE TABLE IF NOT EXISTS example (" +
-                "id int PRIMARY KEY AUTO_INCREMENT," +
-                "name varchar(100) NOT NULL," +
-                "age int NOT NULL" +
-                ")"
-			);
-            mySql.Query("INSERT INTO example (name, age) VALUES ('ignatz', 55)");
-            mySql.Query("INSERT INTO example (name, age) VALUES ('asdf', 22)");
+            // Query statements
+            // You can use Verbatim literals (@) for multiline statements
 
+            mySql.Query(@"CREATE TABLE IF NOT EXISTS example (
+                        id int PRIMARY KEY AUTO_INCREMENT,
+                        name varchar(100) NOT NULL,
+                        age int NOT NULL)"
+                        );
+                        
+            mySql.Query(@"INSERT INTO example (name, age)
+                        VALUES ('ignatz', 55)"
+                        );
+
+            mySql.Query(@"INSERT INTO example (name, age)
+                        VALUES ('asdf', 22)"
+                        );
+
+            // With Prepared Statements
+            mySql.PreparedQuery("new name", 30);
+
+            // Get data from database and print
             mySql.PrintAll();
-
         }
     }
 }
--- db/MySqlConnector.cs
+++ db/MySqlConnector.cs
@@ -8,10 +8,17 @@ namespace csharp_mysql_connection
         private MySqlConnection SQL;
 
         public MySqlConnector(string server, string db, string user_id, string user_pass) {
-            string connection = "Server = " + server + "; Database = " + db + "; Uid = " + user_id + "; Pwd = " + user_pass;
+            string connection =
+                "Server = " + server + "; "
+                + "Database = " + db + "; "
+                + "Uid = " + user_id + "; "
+                + "Pwd = " + user_pass;
 
-            this.SQL = new MySqlConnection();
-            SQL.ConnectionString = connection;
+            this.SQL = new MySqlConnection(connection);
+
+            // Alternative:
+            // this.SQL = new MySqlConnection();
+            // SQL.ConnectionString = connection;
         }
 
         public void Query(string query)
@@ -23,16 +30,37 @@ namespace csharp_mysql_connection
             SQL.Close();
         }
 
+        public void PreparedQuery(string name, int age)
+        {
+            // Don't put @ placeholders in quotes (name)
+            string query = "INSERT INTO example (name, age) VALUES (@name, @age)";
+            MySqlCommand command = new MySqlCommand(query, SQL);
+
+            // Add values
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@age", age);
+
+            // Open connection before Prepare()
+            SQL.Open();
+            command.Prepare();
+            command.ExecuteNonQuery();
+            SQL.Close();
+        }
+
         public void PrintAll()
         {
             string query = "SELECT * FROM example";
             MySqlCommand command = new MySqlCommand(query, SQL);
 
+            // Open connection before ExecuteReader()
             SQL.Open();
             MySqlDataReader data = command.ExecuteReader();
 
             while (data.Read()) {
-                Console.WriteLine(data.GetString(0) + ", " + data.GetString(1) + ", " + data.GetString(2));
+                Console.WriteLine(
+                    data.GetString(0) + ", "
+                    + data.GetString(1) + ", "
+                    + data.GetString(2));
             }
             SQL.Close();
         }
